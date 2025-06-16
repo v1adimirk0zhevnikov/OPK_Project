0307ee83e39b75ff766311891f3be1786b0f43a6
Valdemar <konerlol@mail.ru>
2025-06-16 20:15:13 +0300

    Commit 3D vizualization

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-from src.em_algorithm import em_alg, mu
+from src.em_algorithm import em_alg
 from src.preaparing_data import create_skips
 from src.vizualization import *
 from src.mse_for_em import mse
@@ -18,9 +18,15 @@ mask = data_with_skips.isna().values
 
 #====VIZUAL WITH MATPLOTLIB====#
 mse_values: np.ndarray = np.zeros(20)
-for j in range(20):
-    data_with_skips = em_alg(data_with_skips, mask, epochs=1)
-    mse_values[j] = mse(data_with_skips, dataset, mask)
+df: pd.DataFrame = pd.DataFrame()
+
+for probablity in [x / 100 for x in range(10, 31)]:
+    data_with_skips: pd.DataFrame = create_skips(dataset, probablity)
+    mask = data_with_skips.isna().values
+    for j in range(20):
+        data_with_skips = em_alg(data_with_skips, mask, epochs=1)
+        mse_values[j] = mse(data_with_skips, dataset, mask)
+    df[str(probablity)] = mse_values
 
 
-mse_vizualization(mse_values)
+mse_at_diff_probablity(df)
--- src/vizualization.py
+++ src/vizualization.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 
 
 def mse_vizualization(values: np.ndarray):
@@ -11,5 +12,26 @@ def mse_vizualization(values: np.ndarray):
     plt.ylabel("MSE")
     plt.xticks(x)
     plt.title("Mean squared error")
+
+    plt.show()
+
+
+def mse_at_diff_probablity(prob_values: pd.DataFrame):
+
+    X, Y = np.meshgrid(np.arange(20) + 1, [x / 100 for x in range(10, 31)])
+    Z = prob_values.T
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection="3d")
+
+    # Creating surface:
+    ax.plot_surface(X, Y, Z, cmap="viridis")
+
+    ax.set_xlabel('Iteration')
+    ax.set_ylabel('Probablity of skip')
+    ax.set_zlabel('MSE')
+
+    ax.view_init(elev=30, azim=-30)
+
+    plt.title("Dependence MSE on probablity of skip and quantity of iterations")
 
     plt.show()
